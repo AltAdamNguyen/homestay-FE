f599d1c0df0b37399e3906dab419c32448ea5c4f
anhnc13 <anhnc13@fpt.com>
2025-07-28 17:38:31 +0700

    Initial project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.less$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+                'less-loader'
+            ]
+        })
+        return config;
+    }
 };
 
 export default nextConfig;
---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUS: React.FC = () => {
+    return (
+        <div>about</div>
+    )
+}
+
+export default AboutUS
